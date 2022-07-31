e2359e774ba62fcc904a7b29f909ec594866c0b7
AlTeleg <alteleg@yandex.ru>
2022-07-31 23:08:20 +0300

    Initial commit

---
+++ calculator/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+def recipe_show(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    if dish in DATA:
+        context = {
+            'recipe': {key: value*servings for key, value in DATA.get(dish).items()}
+        }
+        return render(request, 'calculator/index.html', context)
+    else:
+        return HttpResponse('Блюдо не найдено!')
+
+
+
+
